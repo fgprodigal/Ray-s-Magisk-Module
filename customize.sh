7946c4ab21e869480a7b807cf90455a5aeee5237
fgprodigal <fgprodigal@gmail.com>
2020-03-25 00:16:23 +0800

    Revert "Update"
    This reverts commit d5257e5b7b7102ce144e314413d4b0ef39540dd3.

--- customize.sh
+++ customize.sh
@@ -1,70 +1,9 @@
-##########################################################################################
-#
-# MMT Extended Config Script
-#
-##########################################################################################
+# 注意 这不是占位符！！修改权限的时候请把0改为1
+SKIPUNZIP=1
 
-##########################################################################################
-# Config Flags
-##########################################################################################
+ui_print "- 正在提取模块文件"
+unzip -o "$ZIPFILE" -x 'META-INF/*' -d $MODPATH >&2
 
-# Uncomment and change 'MINAPI' and 'MAXAPI' to the minimum and maximum android version for your mod
-# Uncomment DYNLIB if you want libs installed to vendor for oreo+ and system for anything older
-# Uncomment DEBUG if you want full debug logs (saved to /sdcard)
-#MINAPI=21
-#MAXAPI=25
-#DYNLIB=true
-#DEBUG=true
-
-##########################################################################################
-# Replace list
-##########################################################################################
-
-# List all directories you want to directly replace in the system
-# Check the documentations for more info why you would need this
-
-# Construct your list in the following format
-# This is an example
-REPLACE_EXAMPLE="
-/system/app/Youtube
-/system/priv-app/SystemUI
-/system/priv-app/Settings
-/system/framework
-"
-
-# Construct your own list here
-REPLACE="
-/system/etc/permissions/services.cn.google.xml
-/system/etc/permissions/com.oem.china.xml
-/system/product/etc/permissions/services.cn.google.xml
-/system/product/etc/permissions/com.oem.china.xml
-"
-
-##########################################################################################
-# Permissions
-##########################################################################################
-
-set_permissions() {
-  : # Remove this if adding to this function
-
-  # Note that all files/folders in magisk module directory have the $MODPATH prefix - keep this prefix on all of your files/folders
-  # Some examples:
-  
-  # For directories (includes files in them):
-  # set_perm_recursive  <dirname>                <owner> <group> <dirpermission> <filepermission> <contexts> (default: u:object_r:system_file:s0)
-  
-  # set_perm_recursive $MODPATH/system/lib 0 0 0755 0644
-  # set_perm_recursive $MODPATH/system/vendor/lib/soundfx 0 0 0755 0644
-
-  # For files (not in directories taken care of above)
-  # set_perm  <filename>                         <owner> <group> <permission> <contexts> (default: u:object_r:system_file:s0)
-  
-  # set_perm $MODPATH/system/lib/libart.so 0 0 0644
-  # set_perm /data/local/tmp/file.txt 0 0 644
-}
-
-##########################################################################################
-# MMT Extended Logic - Don't modify anything after this
-##########################################################################################
-
-SKIPUNZIP=0
+set_perm_recursive $MODPATH 0 0 0755 0644
+set_perm  $MODPATH/system/etc/permissions/services.cn.google.xml 0 0 0000 0000
+set_perm  $MODPATH/system/product/etc/permissions/services.cn.google.xml 0 0 0000 0000
